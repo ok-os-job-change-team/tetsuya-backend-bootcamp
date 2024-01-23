39f92d26dee7024195710f2c03fb94930d3014c8
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-01-23 14:32:31 +0900

    bulk insertに変更

--- mysql/test.sql
+++ mysql/test.sql
@@ -2,7 +2,7 @@
 CREATE TABLE test.users (id INT AUTO_INCREMENT, name TEXT, PRIMARY KEY(id));
 
 -- usersに挿入
-INSERT INTO test.users(name) values('John');
-INSERT INTO test.users(name) values('Paul');
-INSERT INTO test.users(name) values('George');
-INSERT INTO test.users(name) values('Ringo');
+INSERT INTO test.users(name) values('John'), ('Paul'), ('George'), ('Ringo');
+
+-- usersテーブルの内容を表示
+SELECT * FROM users;
