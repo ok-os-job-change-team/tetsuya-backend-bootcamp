66b68371f9bd25e7825c3388946af3f9371d88b6
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-01-27 22:12:07 +0900

    インデント調整、values->VALUES

--- mysql/user_table.sql
+++ mysql/user_table.sql
@@ -5,12 +5,12 @@ CREATE TABLE users (
   password VARCHAR(20),
   created_at DATETIME,
   updated_at DATETIME
-  );
+);
 
 -- usersに挿入
 INSERT INTO users
   (id, email, password, created_at, updated_at)
-values
+VALUES
   (1, 'hoge@example.com', 'hogehoge', '2024-01-01 01:11:11', '2024-01-01 01:11:11'),
   (2, 'piyo@example.com', 'piyopiyo', '2024-01-02 02:22:22', '2024-01-02 02:22:22'),
   (3, 'foo@example.com', 'foofoo', '2024-01-03 03:33:33', '2024-01-03 03:33:33'),
