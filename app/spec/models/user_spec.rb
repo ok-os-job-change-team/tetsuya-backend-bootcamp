5d5554f30ebaaff0e700c909254b710acae79028
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-01-31 13:56:50 +0900

    user_spec.rbの修正

--- app/spec/models/user_spec.rb
+++ app/spec/models/user_spec.rb
@@ -1,17 +1,18 @@
-require_relative '../../user'
+require_relative './user' #user.rbの相対パス
 
 RSpec.describe User do
-  describe '#id' do
+  describe '#present?' do
     context 'idがnilでない場合' do
-      it 'present?がtrue' do
-        john = User.new(1, 'john@example.com', 'p@ssw0rd')
-        expect(john.present?).to eq true
+      it 'present?がtrueになる' do
+        user = User.new(1, 'user@example.com', 'p@ssw0rd')
+        expect(user.present?).to eq true
       end
     end
+    
     context 'idがnilの場合' do
-      it 'present?がfalse' do
-        john = User.new(nil, 'john@example.com', 'p@ssw0rd')
-        expect(john.present?).to eq false
+      it 'present?がfalseになる' do
+        user = User.new(nil, 'user@example.com', 'p@ssw0rd')
+        expect(user.present?).to eq false
       end
     end
   end
