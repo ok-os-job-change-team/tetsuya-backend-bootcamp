c20d57a000d009b6063d3f865120fcc8392ba716
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-02-27 10:37:35 +0900

    feat: favoriteクラスを実装する

---
+++ app/models/favorite.rb
@@ -0,0 +1,19 @@
+require_relative 'user'
+require_relative 'tweet'
+
+class Favorite
+  attr_reader :id, :user_id, :tweet_id
+  def initialize(id:, user_id:, tweet_id:)
+    @id = id
+    @user_id = user_id
+    @tweet_id = tweet_id
+  end
+
+  def present?
+    @id != nil
+  end
+
+  def self.build_by_user_and_tweet(user:, tweet:, id:)
+    Favorite.new(id: id, user_id: user.id, tweet_id: tweet.id)
+  end
+end
---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../../app/models/user' #user.rbの相対パス
+require_relative '../../app/models/tweet' #tweet.rbの相対パス
+require_relative '../../app/models/favorite' #favorite.rbの相対パス
+
+
+RSpec.describe Favorite do
+  describe '#present?' do
+    context 'idがnilでない場合' do
+      it 'present?がtrueになる' do
+        favorite = Favorite.new(id: 1, user_id: 22, tweet_id: 333)
+        expect(favorite.present?).to eq true
+      end
+    end
+    
+    context 'idがnilの場合' do
+      it 'present?がfalseになる' do
+        favorite = Favorite.new(id: nil, user_id: 22, tweet_id: 333)
+        expect(favorite.present?).to eq false
+      end
+    end
+  end
+
+  describe '#self.build_by_user_and_tweet' do
+    context 'Userクラスのインスタンスにid = 1を、Tweetクラスのインスタンスにid = 22を渡すとき' do
+      it 'favorite.user_idが1を、favorite.tweet_idが22を返す' do
+        user = User.new(id: 1, email: 'user@example.com', password: 'p@ssw0rd')
+        tweet = Tweet.build_by_user(user: user, id: 22, title: 'タイトル', tweet_content: '本文')
+        favorite = Favorite.build_by_user_and_tweet(user: user, tweet: tweet, id: 111)
+        expect(favorite.user_id).to eq 1
+        expect(favorite.tweet_id).to eq 22
+      end
+    end
+  end
+end
